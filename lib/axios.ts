bf2e4bd5b079ac3b79d4616eaf0b34188fc7ba9b
Habibullah Qureshe <habibullahqureshe@gmail.com>
2025-09-10 20:52:26 -0700

    axios config

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { store } from "@/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // apna backend url
+  baseURL: "/api", 
   withCredentials: true, 
 });
 
-// Har request me token lagao agar available ho
+
 api.interceptors.request.use((config) => {
   const state = store.getState();
   const token = state.auth.token;
